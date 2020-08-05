23c9aa2cdb4c2a21e860cb484dfe27d9d56d821a
Tomas Jasek <tomsik68@posteo.net>
2020-08-05 12:04:06 +0200

    sl: warning for unsupported interval offset by dereferencing array object

--- sl/symheap.cc
+++ sl/symheap.cc
@@ -3628,7 +3628,7 @@ TValId SymHeapCore::valCreate(EValueTarget code, EValueOrigin origin)
 
         case VT_RANGE:
             // VT_RANGE pointing to a freed object is replaced by VT_UNKNOWN
-            code = VT_UNKNOWN;
+            //code = VT_UNKNOWN;
             // fall through!
 
         case VT_UNKNOWN:
